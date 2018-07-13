ee958a2cdd579c2170af79640d939f8475e40708
Nguyễn Thế Tuân <tuannt39@gmail.com>
2018-07-13 13:48:22 +0700

    Java 8 - Default Methods

---
+++ src/vn/its/Java8_05_DefaultMethods.java
@@ -0,0 +1,34 @@
+package vn.its;
+
+public class Java8_05_DefaultMethods {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Car();
+        vehicle.print();
+    }
+
+}
+
+interface Vehicle {
+    default void print() {
+        System.out.println("I am a vehicle!");
+    }
+
+    static void blowHorn() {
+        System.out.println("Blowing horn!!!");
+    }
+}
+
+interface FourWheeler {
+    default void print() {
+        System.out.println("I am a four wheeler!");
+    }
+}
+
+class Car implements Vehicle, FourWheeler {
+    public void print() {
+        Vehicle.super.print();
+        FourWheeler.super.print();
+        Vehicle.blowHorn();
+        System.out.println("I am a car!");
+    }
+}
